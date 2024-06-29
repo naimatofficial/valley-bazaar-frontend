0242c9ea3d3dbcbee5339646c935e04ee8c1765c
Naimat Ullah <naimat.official.10@gmail.com>
2024-06-29 09:56:30 +0500

    add header, topbar, and navbar startup

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Topbar from "./Topbar";
+
+const Header = () => {
+	return (
+		<div>
+			<Topbar />
+			<Navbar />
+		</div>
+	);
+};
+
+export default Header;
